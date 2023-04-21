7e36256b148dd43f87403a7fa45a7a10844fdc0c
anthony <anthonyspiteri@hotmail.fr>
2023-04-21 15:08:29 +0200

    first

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-const form = document.querySelector('form')
-const result = document.querySelector('.result')
-const inputCodePostal = document.querySelector(".codepostal")
+const nbPokemon = document.querySelector('.nbPokemon')
+const list = document.createElement('div')
 
-form.addEventListener('input', (event) => {
-    event.preventDefault();
+
+
+
+
+
+
     
-    getInfosAPI(inputCodePostal)
-})
-
-
-
-
-function getInfosAPI(inputCodePostal){
-    
-
-    fetch("https://geo.api.gouv.fr/communes?codePostal="+inputCodePostal.value+"&fields=nom, communes")
+/*
+    fetch("https://pokeapi.co/api/v2/pokemon-form/")
     .then((response) => response.json())
     .then((data) => {
+        console.log(data.results)
+        nbPokemon.innerHTML = "<ul>"
+        data.results.forEach((element) =>{
+            nbPokemon.innerHTML += "<li> <p>" + element.name.charAt(0).toUpperCase() + element.name.slice(1) + "</p></li>"
+         
+        })
+        nbPokemon.innerHTML += "</ul>"
+     })
+     .catch((error) => nbPokemon.innerHTML = error)*/
+     
+     for( let i = 1; i<= 151; i++){
+     
+    fetch("https://pokeapi.co/api/v2/pokemon-form/"+i)
+    .then((response) => response.json())
+    .then((data) => {
+
         console.log(data)
-        result.innerHTML = " "
-
-        //result.innerHTML = "<option> Nom : " + data[0].nom  + "</option><br>"
-        data.forEach((nomville) => {
-           result.innerHTML += "<option>" + nomville.nom + "</option>"
-           
-     })
        
-     })
-     .catch((error) => result.innerHTML = error)
-}
+      nbPokemon.innerHTML +=  "<div ><h3>"+ data.name.charAt(0).toUpperCase()+data.name.slice(1) +"</h3><img src="+data.sprites.front_default+"><p>Type : "+data.types[0].type.name+"</p></div>"
 
+        switch(data.types[0].type.name){
+            case 'grass':
+        nbPokemon.children.style.background = 'green'
+        break;
+        }
+      
+       
+      
+     })
+     .catch((error) => nbPokemon.innerHTML = error)
+    }
+
+    
